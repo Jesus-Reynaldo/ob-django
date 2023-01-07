e03959f69b8a3ae4997745c99de319b4ffaae396
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2023-01-06 22:00:25 -0400

    first commit

---
+++ holamundo/holamundo/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+def saludo(request):
+  return HttpResponse("Hola mundo")
+def despedida(request):
+  return HttpResponse("Hasta luego")
+def adulto(request,edad):
+  if edad >=18:
+    return HttpResponse("Eres mayor de edad")
+  else:
+    return HttpResponse("No eres mayor de edad")
+  
