ddca7a67002fa849e2893550d291bf4c2dad331c
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2023-01-09 00:42:41 -0400

    Creacion y borrado de datos

--- modularizacion/comentarios/models.py
+++ modularizacion/comentarios/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class comments(models.Model):
+class Comment(models.Model):
   name = models.CharField(max_length=255,null=False)
   score = models.IntegerField(default=3)
   comment = models.TextField(max_length=1000,null=True)
--- modularizacion/comentarios/views.py
+++ modularizacion/comentarios/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Comment
 # Create your views here.
+def test(request):
+  return HttpResponse("Funciona correctamente")
+
+def create(request):
+  #comment = Comment(name="Jesus",score=5,comment="Este es un comentario")
+  #comment.save()
+  comment = Comment.objects.create(name="Alex")
+  return HttpResponse("Ruta para probar la creacion de modelos")
+
+def delete(request):
+  #comment = Comment.objects.get(id=1)
+  #comment.delete()
+  Comment.objects.filter(id=2).delete()
+  return HttpResponse("Ruta para probar el borrado de modelos")
