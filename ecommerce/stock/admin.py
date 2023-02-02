fe1e35785f76ca93f9bf8c1e8aff9919b84f222a
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2023-02-02 00:51:05 -0400

    Uso del ModelAdmin

--- ecommerce/stock/admin.py
+++ ecommerce/stock/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+  #Añadir los atributos de la clase Product
+  list_display = ("name","short_description","stock")
+  #Añadir una barra de busqueda
+  search_fields=("name","short_description",)
+  #Añadir un filtro 
+  list_filter = ("discount_unit","stock",)
+  #Añadir una gerarquia
+  date_hierarchy ="discount_unit"
+
+admin.site.register(Product,ProductAdmin)
