7963a7f1393a4aacc0b2897d549d1b64a982018b
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2023-02-03 17:19:10 -0400

    Practica: Buscador y listas

--- agenda/agenda/urls.py
+++ agenda/agenda/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("contact", include('contact.urls') ),
     path("todo", include('todo.urls') ),
-    
+    path("",views.index,name='index')
 ]
--- agenda/contact/views.py
+++ agenda/contact/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
+def index(request):
+
+  contacts=Contact.objects.filter(name__contains=request.GET.get('search',''))
+  context = {
+    'contacts': contacts
+  }
+  return render(request,'contact/index.html',context)
