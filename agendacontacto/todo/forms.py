24d9771b26b9115250420f5ea81cde2293e49c45
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2023-06-05 17:27:31 -0400

    Creacion del CRUD de tareas

--- agendacontacto/todo/forms.py
+++ agendacontacto/todo/forms.py
@@ -4,5 +4,5 @@ from .models import Todo
 class TodoForm(ModelForm):
   class Meta:
     model = Todo
-    fields= '__all__'
+    exclude = ('date',)
     
--- agendacontacto/todo/urls.py
+++ agendacontacto/todo/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='todo'),
+    path('create/', views.create, name='todo_create'),
+    path('view/<int:id>', views.view, name='todo_view'),
+    path('delete/<int:id>', views.delete, name='todo_delete'),
+    path('edit/<int:id>', views.edit, name='todo_edit'),
 ]
